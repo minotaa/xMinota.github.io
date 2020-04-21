d6fc9399a5c3c498afd741d633684735c068153f
Minota <34592028+xMinota@users.noreply.github.com>
2020-04-21 11:46:48 -0400

    :moon:

---
+++ script.js
@@ -0,0 +1,6 @@
+var scrl = "Fun fact about Minota: He's terrible at web design. ";
+function scrollTitle() {
+	scrl = scrl.substring(1, scrl.length) + scrl.substring(0, 1);
+	document.title = scrl;
+	setTimeout("scrollTitle()", 300);
+}; scrollTitle();
